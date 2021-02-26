502884a02b4093f96ef66d65d356f728ad543ea2
paulatalle <paulatalle@localhost>
2021-02-26 11:28:58 +0100

    @throw

--- timeOfDay_oop_ex/src/exerciseTimeOfDay/TimeOfDayImmut.java
+++ timeOfDay_oop_ex/src/exerciseTimeOfDay/TimeOfDayImmut.java
@@ -16,9 +16,9 @@ public class TimeOfDayImmut {
 	/**
 	 * Initializes this object so that it represents the time of day on a 24-hour m¡litary time.
 	 * 
-	 * @pre The hour can be any value between 0 and 24, the minute can be any value between 0 and 60.
-	 * 		| 0 <= hour <= 24
-	 * 		| 0 <= minute <= 60
+	 * @throws IllegalArgumentException |  0 <= hour <= 24
+	 * @throws IllegalArgumentException | 0 <= minute <= 60
+	 * 
 	 * @post This interval's hour equals the given hour
 	 *     | getHour() == hour
 	 * @post This interval's minute equals the given minute
